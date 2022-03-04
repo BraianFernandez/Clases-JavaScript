06e4680458963ed6e2a29ed051380b6c512328fa
Braian Fernandez <braianjosefernandez@hotmail.com>
2022-03-04 08:51:14 -0300

    Se acomodan los archivos en carpetas y se termina el TP 2

---
+++ tp_n2/tpn2.js
@@ -0,0 +1,18 @@
+var cantidadDeVentanasEnMiCasa = 2
+
+console.log(cantidadDeVentanasEnMiCasa)
+
+var soyHumano = true
+
+console.log(soyHumano)
+
+var miGustoDePizzaPreferido = "pizza de huevos fritos con backon"
+
+console.log(miGustoDePizzaPreferido)
+
+var multiplesValores = [false, "hola", 0, "Minions"]
+
+for (var i = 0; i < multiplesValores.length; i++) {
+    console.log(multiplesValores[i])
+    
+}
