783a0a7257f79b0e56e57820f3d40a66f00bf712
Braian Fernandez <braianjosefernandez@hotmail.com>
2022-03-04 09:27:49 -0300

    Se termino el TP 3-1

---
+++ tpN3/tpN3.js
@@ -0,0 +1,77 @@
+var hoy = new Date();
+
+
+// 1
+var noValgoNi5 = 4
+console.log(noValgoNi5)
+
+
+//2
+var miAnioDeNacimiento = 1994
+var meGustariaTener80Anios = 80
+var voyATener80ElAnio = miAnioDeNacimiento + meGustariaTener80Anios
+
+console.log(voyATener80ElAnio)
+
+
+//3
+var costoDeUnaTele = 10000
+var ahorros = 9000
+var dineroQueMeFalta = costoDeUnaTele - ahorros
+
+console.log(dineroQueMeFalta)
+
+
+//4
+var diasPorAnio = 365
+var cantidadDeAniosEnUnaDecada = 10
+var cantidadDeDiasEnUnaDecada = diasPorAnio * cantidadDeAniosEnUnaDecada
+
+console.log(cantidadDeDiasEnUnaDecada)
+
+
+//5
+var porcionesPizza = 8
+var porcionesQueTengo = 24
+var TengoPizzas = porcionesQueTengo / porcionesPizza
+
+console.log(TengoPizzas)
+ 
+
+//6
+var miAanimalFavorito = "Lobo"
+console.log(miAanimalFavorito == "perro")
+
+
+//7 
+console.log("flan" != "flan Con Dulce de Leche")
+
+
+//8 
+var soyMayorDeEdad = 27 > 17
+console.log(soyMayorDeEdad)
+
+
+//9 
+var soyMenorDe25 = 27 < 25
+console.log(soyMenorDe25)
+
+
+//10
+var estamosEnEnero = ("marzo" === "Enero")
+console.log(estamosEnEnero)
+
+
+//11
+var NoEstamosEnEnero = ("Marzo" !== "Enero")
+console.log(NoEstamosEnEnero)
+
+//12
+var miNotaEnElParcial = 8
+var notaMinimaParaAprobar = 6
+var estoyAprobado = miNotaEnElParcial >= notaMinimaParaAprobar
+console.log(estoyAprobado)
+
+//13
+var esHoraDeAlmorzar = hoy.getHours() <= 12
+console.log(esHoraDeAlmorzar)
